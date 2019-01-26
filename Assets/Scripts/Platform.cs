006ce7e8114e4d0aaa5c1de3d948bad054061e2f
Fabpk90 <fabrizio1996@live.fr>
2019-01-27 00:37:12 +0100

    Moving platform

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -1,5 +1,28 @@
 
+using UnityEngine;
+
 public class Platform : Deathable
 {
+    public bool hasToMove;
     
+    public GameObject pointA;
+    public GameObject pointB;
+
+    private bool goingToA;
+    
+    private void FixedUpdate()
+    {
+        if (hasToMove)
+        {
+            var moving = Vector3.MoveTowards(transform.position
+                             , goingToA
+                                 ? pointA.transform.position
+                                 : pointB.transform.position, 0f) * Time.deltaTime;
+
+            if (moving == transform.position)
+                goingToA = !goingToA;
+            else
+                transform.position = moving;
+        }
+    }
 }
