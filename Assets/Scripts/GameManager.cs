2dced1267d7ea06bec7dbf6c76f5707e0908693e
Unknown <timothy.bellaiche@gmail.com>
2019-01-26 04:59:30 +0100

    Finished movement, jump & dash. Started attack

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Player player;
+    public bool gameHasStarted = false;
+    public GameObject titleScreenUi;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameHasStarted)
+        {
+            if (Input.anyKey)
+            {
+                gameHasStarted = true;
+                Debug.Log("Game Started");
+                titleScreenUi.SetActive(false);
+                player.hasControl = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerAttackHitbox.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackHitbox : MonoBehaviour
+{
+    private int damage;
+    private float duration;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        damage = GetComponentInParent<Player>().attackDamage;
+        StartCoroutine(SelfDestruct());
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag== "Enemy")
+        {
+            // TODO : Remove health of enemy
+            Destroy(gameObject);
+        }
+    }
+
+    private IEnumerator SelfDestruct()
+    {
+        yield return new WaitForSeconds(duration);
+        Destroy(gameObject);
+    }
+}
